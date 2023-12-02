e5bc572cd0d96f767cb567ae6cf195eb6918502b
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-02 12:50:36 +0330

    Update Start.cs

--- OODLessonProject/Start.cs
+++ OODLessonProject/Start.cs
@@ -15,6 +15,20 @@ namespace OODLessonProject
         public Start()
         {
             InitializeComponent();
+            LoadProejctList();
+        }
+
+        private void LoadProejctList()
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.Cells.AddRange(new DataGridViewCell[] {
+            new DataGridViewTextBoxCell{ Value = "عبارت پسوندی با استک",},
+            new DataGridViewTextBoxCell{ Value = " با استفاده از کالس Stack طراحی شده در تمرین قبل برنامهای به زبان #C بنویسید که یک عبارت میانوندی 
+را با استفاده از پشته به یک عبارت پسوندی تبدیل کند )الگوریتم تبدیل در ابتدای این جزوه ارائه شده است(."},
+            new DataGridViewTextBoxCell{ Value = "Hi"},
+            });
+
+            dgvProjects.Rows.Add(row);
         }
     }
 }
