83470b4c8f92019685c00a617d32192f678e01c1
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 02:02:39 +0330

    Update Start.cs

--- OODLessonProject/Start.cs
+++ OODLessonProject/Start.cs
@@ -22,13 +22,28 @@ namespace OODLessonProject
             dgvProjects.Rows.Add(row1);
 
             DataGridViewRow row2 = new DataGridViewRow();
-            row2.Height = 54;
             row2.Cells.AddRange(new DataGridViewCell[] {
             new DataGridViewTextBoxCell{ Value = "تمرین 2",},
             new DataGridViewTextBoxCell{ Value = "کلاسی به نام Circle برای دایره طراحی کنید. این کالس باید دارای یک سازنده پیش فرض، یک سازنده که شعاع دایره را به عنوان پارامتر دریافت میکند، یک سازنده کپی و توابع محاسبه محیط و مساحت باشد."},
             new DataGridViewTextBoxCell{ Value = "27"},
             });
             dgvProjects.Rows.Add(row2);
+
+            DataGridViewRow row3 = new DataGridViewRow();
+            row2.Cells.AddRange(new DataGridViewCell[] {
+            new DataGridViewTextBoxCell{ Value = "تمرین 3",},
+            new DataGridViewTextBoxCell{ Value = "به آرایه های دو بعدی ماتریس گفته میشود. هر ماتریس دارای تعدادی سطر و تعدادی ستون میباشد.کلاسی به نام Matrix برای مدل سازی یک ماتریس دو بعدی طراحی کنید. برای این کلاس سازنده های مورد نیاز و همچنین یک سازنده کپی پیاده سازی کنید."},
+            new DataGridViewTextBoxCell{ Value = "27"},
+            });
+            dgvProjects.Rows.Add(row3);
+
+            DataGridViewRow row4 = new DataGridViewRow();
+            row4.Cells.AddRange(new DataGridViewCell[] {
+            new DataGridViewTextBoxCell{ Value = "تمرین 4",},
+            new DataGridViewTextBoxCell{ Value = ""},
+            new DataGridViewTextBoxCell{ Value = "27"},
+            });
+            dgvProjects.Rows.Add(row4);
         }
 
         private void BOpen_Click(object sender, EventArgs e)
