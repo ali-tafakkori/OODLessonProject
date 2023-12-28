3eb269cb0d615620cec58bb53107dd2b1e4c24c7
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-28 18:31:26 +0330

    Create Circle.cs

---
+++ OODLessonProject/P2/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OODLessonProject.P2
+{
+    class Circle
+    {
+        private double radius;
+
+        public Circle()
+        {
+            radius = 0;
+        }
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public Circle(Circle other)
+        {
+            radius = other.radius;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
