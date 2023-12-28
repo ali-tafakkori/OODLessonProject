49b31e3d4dcccfd7a87308269b8dbe21e1671665
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-28 18:12:35 +0330

    Update P1Form.cs

--- OODLessonProject/P1Form.cs
+++ OODLessonProject/P1Form.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 using System.Windows.Forms;
 
 namespace OODLessonProject
@@ -15,6 +9,88 @@ namespace OODLessonProject
         public P1Form()
         {
             InitializeComponent();
+        }
+        private void BConvert_Click(object sender, System.EventArgs ev)
+        {
+            string input = tbInput.Text;
+            try {
+                lOutput.Text = Convert(input);
+            }
+            catch (Exception e) {
+                lOutput.Text = e.ToString();
+            }
+        }
+
+        string Convert(string expression) {
+            string[] tokens = expression.Split(' ');
+
+            Stack operators = new Stack();
+            Queue postfix = new Queue();
+
+            foreach (string token in tokens)
+            {
+                if (IsOperator(token))
+                {
+                    while (operators.Count > 0 && IsHigherPrecedence((string)operators.Peek(), token))
+                    {
+                        postfix.Enqueue(operators.Pop());
+                    }
+                    operators.Push(token);
+                }
+                else if (token == "(")
+                {
+                    operators.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while ((string)operators.Peek() != "(")
+                    {
+                        postfix.Enqueue(operators.Pop());
+                    }
+                    operators.Pop();
+                }
+                else
+                {
+                    postfix.Enqueue(token);
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                postfix.Enqueue(operators.Pop());
+            }
+
+            return string.Join(" ", postfix.ToArray());
+        }
+
+         bool IsOperator(string token)
+        {
+            return token == "+" || token == "-" || token == "*" || token == "/" || token == "^";
+        }
+
+         bool IsHigherPrecedence(string op1, string op2)
+        {
+            int op1Precedence = GetOperatorPrecedence(op1);
+            int op2Precedence = GetOperatorPrecedence(op2);
+
+            return op1Precedence >= op2Precedence;
+        }
+
+         int GetOperatorPrecedence(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                case "^":
+                    return 3;
+                default:
+                    return 0;
+            }
         }
     }
 }
