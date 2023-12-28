964ef1ec6922b648da1b704e1626c83fd97ea3d2
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-28 19:41:15 +0330

    Update P2Form.Designer.cs

--- OODLessonProject/P2/P2Form.Designer.cs
+++ OODLessonProject/P2/P2Form.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace OODLessonProject
+namespace OODLessonProject.P2
 {
     partial class P2Form
     {
@@ -31,7 +31,11 @@ namespace OODLessonProject
         {
             this.lTitle = new System.Windows.Forms.Label();
             this.lDesc = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tbRadius = new System.Windows.Forms.TextBox();
+            this.bCalculate = new System.Windows.Forms.Button();
+            this.lEnterRadius = new System.Windows.Forms.Label();
+            this.lPerimeter = new System.Windows.Forms.Label();
+            this.lArea = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lTitle
@@ -56,20 +60,58 @@ namespace OODLessonProject
     " یک سازنده که شعاع دایره را به عنوان پارامتر دریافت میکند، یک سازنده کپی و توابع" +
     " محاسبه محیط و مساحت باشد.";
             // 
-            // richTextBox1
+            // tbRadius
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 129);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 309);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.tbRadius.Location = new System.Drawing.Point(323, 202);
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(100, 20);
+            this.tbRadius.TabIndex = 5;
+            // 
+            // bCalculate
+            // 
+            this.bCalculate.Location = new System.Drawing.Point(348, 228);
+            this.bCalculate.Name = "bCalculate";
+            this.bCalculate.Size = new System.Drawing.Size(75, 23);
+            this.bCalculate.TabIndex = 7;
+            this.bCalculate.Text = "Calculate";
+            this.bCalculate.UseVisualStyleBackColor = true;
+            this.bCalculate.Click += new System.EventHandler(this.BCalculate_Click);
+            // 
+            // lEnterRadius
+            // 
+            this.lEnterRadius.AutoSize = true;
+            this.lEnterRadius.Location = new System.Drawing.Point(320, 186);
+            this.lEnterRadius.Name = "lEnterRadius";
+            this.lEnterRadius.Size = new System.Drawing.Size(66, 13);
+            this.lEnterRadius.TabIndex = 4;
+            this.lEnterRadius.Text = "Enter radius:";
+            // 
+            // lPerimeter
+            // 
+            this.lPerimeter.AutoSize = true;
+            this.lPerimeter.Location = new System.Drawing.Point(320, 272);
+            this.lPerimeter.Name = "lPerimeter";
+            this.lPerimeter.Size = new System.Drawing.Size(0, 13);
+            this.lPerimeter.TabIndex = 8;
+            // 
+            // lArea
+            // 
+            this.lArea.AutoSize = true;
+            this.lArea.Location = new System.Drawing.Point(320, 296);
+            this.lArea.Name = "lArea";
+            this.lArea.Size = new System.Drawing.Size(0, 13);
+            this.lArea.TabIndex = 9;
             // 
             // P2Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lArea);
+            this.Controls.Add(this.lPerimeter);
+            this.Controls.Add(this.bCalculate);
+            this.Controls.Add(this.tbRadius);
+            this.Controls.Add(this.lEnterRadius);
             this.Controls.Add(this.lDesc);
             this.Controls.Add(this.lTitle);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
@@ -89,6 +131,10 @@ namespace OODLessonProject
         #endregion
         private System.Windows.Forms.Label lTitle;
         private System.Windows.Forms.Label lDesc;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox tbRadius;
+        private System.Windows.Forms.Button bCalculate;
+        private System.Windows.Forms.Label lEnterRadius;
+        private System.Windows.Forms.Label lPerimeter;
+        private System.Windows.Forms.Label lArea;
     }
 }
