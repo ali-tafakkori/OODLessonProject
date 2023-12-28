6d72a0411d8c697ecf52325e6ba8a2df25fffd17
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-28 18:28:50 +0330

    Create P2Form.Designer.cs

---
+++ OODLessonProject/P2/P2Form.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace OODLessonProject
+{
+    partial class P2Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lTitle = new System.Windows.Forms.Label();
+            this.lDesc = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.Font = new System.Drawing.Font("B Zar", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lTitle.Location = new System.Drawing.Point(723, 9);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(65, 35);
+            this.lTitle.TabIndex = 1;
+            this.lTitle.Text = "تمرین 2";
+            // 
+            // lDesc
+            // 
+            this.lDesc.Font = new System.Drawing.Font("B Zar", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lDesc.Location = new System.Drawing.Point(12, 44);
+            this.lDesc.Name = "lDesc";
+            this.lDesc.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lDesc.Size = new System.Drawing.Size(776, 82);
+            this.lDesc.TabIndex = 2;
+            this.lDesc.Text = "کلاسی به نام Circle برای دایره طراحی کنید. این کلاس باید دارای یک سازنده پیش فرض،" +
+    " یک سازنده که شعاع دایره را به عنوان پارامتر دریافت میکند، یک سازنده کپی و توابع" +
+    " محاسبه محیط و مساحت باشد.";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 129);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 309);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // P2Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lDesc);
+            this.Controls.Add(this.lTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "P2Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "P1 Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Label lDesc;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
