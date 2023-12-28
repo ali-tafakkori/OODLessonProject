3c90235609963f7b45cf322bd5fafbc36ed513b2
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-28 17:54:11 +0330

    Create P1Form.cs

---
+++ OODLessonProject/P1Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OODLessonProject
+{
+    public partial class P1Form : Form
+    {
+        public P1Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
