313710c62befcc56ff873b485e6855774acdf5e5
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 16:38:03 +0330

    Update Line.cs

--- OODLessonProject/P4/Line.cs
+++ OODLessonProject/P4/Line.cs
@@ -1,6 +1,31 @@
-﻿namespace OODLessonProject.P4
+﻿using System;
+
+namespace OODLessonProject.P4
 {
     class Line
     {
+        private Point p1, p2;
+
+        public Line()
+        {
+        }
+        public Line(Line other)
+        {
+            p1 = new Point(other.p1);
+            p2 = new Point(other.p2);
+        }
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+            }
+        }
     }
 }
