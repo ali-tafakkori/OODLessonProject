774a871155056ca5e5de0d8f7442df7d0a5efd7a
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 17:43:12 +0330

    Update P4Form.cs

--- OODLessonProject/P4/P4Form.cs
+++ OODLessonProject/P4/P4Form.cs
@@ -17,7 +17,7 @@ namespace OODLessonProject.P4
             string y2 = tbY2.Text;
             try
             {
-                Line line = new Line(new Point(int.Parse(x1),int.Parse(y1)), new Point(int.Parse(x2), int.Parse(y2)));
+                Line line = new Line(new Point(int.Parse(x1), int.Parse(y1)), new Point(int.Parse(x2), int.Parse(y2)));
 
                 lLength.Text = "Length: " + line.Length;
             }
@@ -38,6 +38,27 @@ namespace OODLessonProject.P4
                 Line line = new Line(new Point(int.Parse(x1), int.Parse(y1)), new Point(int.Parse(x2), int.Parse(y2)));
 
                 lSlope.Text = "Slope: " + line.Slope;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void BIsOn_Click(object sender, EventArgs ev)
+        {
+            string x1 = tbX1.Text;
+            string y1 = tbY1.Text;
+            string x2 = tbX2.Text;
+            string y2 = tbY2.Text;
+
+            string xx = tbXX.Text;
+            string yx = tbYX.Text;
+            try
+            {
+                Line line = new Line(new Point(int.Parse(x1), int.Parse(y1)), new Point(int.Parse(x2), int.Parse(y2)));
+
+                lIsOn.Text = "Is on the Line: " + (line.IsOn(new Point(int.Parse(xx), int.Parse(yx))) ? "Yes" : "No");
             }
             catch (Exception e)
             {
