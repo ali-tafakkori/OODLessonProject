080ac62454d229f452b04cec7ea6ae6a856c309d
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 18:26:40 +0330

    Update Polygon.cs

--- OODLessonProject/P5/Polygon.cs
+++ OODLessonProject/P5/Polygon.cs
@@ -1,6 +1,31 @@
-﻿namespace OODLessonProject.P5
+﻿using System.Collections.Generic;
+
+namespace OODLessonProject.P5
 {
     class Polygon
     {
+        private List<Point> points;
+
+        public Polygon(List<Point> points)
+        {
+            this.points = new List<Point>();
+
+            foreach (Point p in points)
+            {
+                this.points.Add(p);
+            }
+        }
+
+        public Point this[int i]
+        {
+            get
+            {
+                return points[i];
+            }
+            set
+            {
+                points[i] = value;
+            }
+        }
     }
 }
