09d47cbab8b85450a39cb586d2f8601b6f92ac67
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 17:32:28 +0330

    Update P4Form.cs

--- OODLessonProject/P4/P4Form.cs
+++ OODLessonProject/P4/P4Form.cs
@@ -26,5 +26,23 @@ namespace OODLessonProject.P4
                 MessageBox.Show(e.ToString());
             }
         }
+
+        private void BSlope_Click(object sender, EventArgs ev)
+        {
+            string x1 = tbX1.Text;
+            string y1 = tbY1.Text;
+            string x2 = tbX2.Text;
+            string y2 = tbY2.Text;
+            try
+            {
+                Line line = new Line(new Point(int.Parse(x1), int.Parse(y1)), new Point(int.Parse(x2), int.Parse(y2)));
+
+                lSlope.Text = "Slope: " + line.Slope;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
     }
 }
