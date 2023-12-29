a07930e79ffb03765b4d9cdfe4f9be2f3f3e9739
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 18:26:38 +0330

    Create Point.cs

---
+++ OODLessonProject/P5/Point.cs
@@ -0,0 +1,44 @@
+﻿namespace OODLessonProject.P5
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+
+        public Point(Point other)
+        {
+            this.x = other.x;
+            this.y = other.y;
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+    }
+}
