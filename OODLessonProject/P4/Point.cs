f75943b905ef2b0a844c40c7fac084e673745492
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-29 16:28:46 +0330

    Update Point.cs

--- OODLessonProject/P4/Point.cs
+++ OODLessonProject/P4/Point.cs
@@ -2,5 +2,37 @@
 {
     class Point
     {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
     }
 }
