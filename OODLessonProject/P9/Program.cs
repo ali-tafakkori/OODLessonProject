3ffbc7c7d487f5b8e4cf564fa0275a7b978d8fa6
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:26:23 +0330

    Create Program.cs

---
+++ OODLessonProject/P9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OODLessonProject.P9
+{
+    class Program
+    {
+        private static int count = 0;
+
+        private string path;
+
+        public Program(string path)
+        {
+            if (count > 1) 
+                throw new Exception("Can't create twice");
+            count++;
+
+            this.path = path;
+        }
+    }
+}
