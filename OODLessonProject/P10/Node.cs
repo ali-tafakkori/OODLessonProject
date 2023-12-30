21b5096a30de29ae71e01f95d586a42aa19b5a24
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:30:26 +0330

    Create Node.cs

---
+++ OODLessonProject/P10/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace OODLessonProject.P10
+{
+    public class Node
+    {
+        private static int idCounter = 100;
+
+        private int data;
+        private int id;
+
+        public Node(int data)
+        {
+            this.data = data;
+            id = idCounter++;
+        }
+    }
+}
