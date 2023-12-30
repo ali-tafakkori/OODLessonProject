828edb626d11384b652a6b69c39f2242d8134d04
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:07:42 +0330

    Create LinkedList.cs

---
+++ OODLessonProject/P7/LinkedList.cs
@@ -0,0 +1,24 @@
+﻿namespace OODLessonProject.P7
+{
+    public class LinkedList
+    {
+        private Node first;
+        private Node last;
+
+        public void Add(Student data)
+        {
+            Node newNode = new Node(data);
+            if (first == null)
+            {
+                first = newNode;
+                last = first;
+            }
+            else
+            {
+                last.Next = newNode;
+                last = newNode;
+            }
+            last.Next = first;
+        }
+    }
+}
