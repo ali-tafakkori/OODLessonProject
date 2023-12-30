336d42a8804dd2a0547b4cbc755153da41d980b1
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:37:10 +0330

    P9Form

---
+++ OODLessonProject/P9/P9Form.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace OODLessonProject.P9
+{
+    partial class P9Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(P9Form));
+            this.lTitle = new System.Windows.Forms.Label();
+            this.lDesc = new System.Windows.Forms.Label();
+            this.bCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.Font = new System.Drawing.Font("B Zar", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lTitle.Location = new System.Drawing.Point(723, 9);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(65, 35);
+            this.lTitle.TabIndex = 1;
+            this.lTitle.Text = "تمرین 9";
+            // 
+            // lDesc
+            // 
+            this.lDesc.Font = new System.Drawing.Font("B Zar", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lDesc.Location = new System.Drawing.Point(12, 44);
+            this.lDesc.Name = "lDesc";
+            this.lDesc.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lDesc.Size = new System.Drawing.Size(776, 114);
+            this.lDesc.TabIndex = 2;
+            this.lDesc.Text = resources.GetString("lDesc.Text");
+            // 
+            // bCreate
+            // 
+            this.bCreate.Location = new System.Drawing.Point(344, 259);
+            this.bCreate.Name = "bCreate";
+            this.bCreate.Size = new System.Drawing.Size(75, 23);
+            this.bCreate.TabIndex = 4;
+            this.bCreate.Text = "Create";
+            this.bCreate.UseVisualStyleBackColor = true;
+            this.bCreate.Click += new System.EventHandler(this.BCreate_Click);
+            // 
+            // P9Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bCreate);
+            this.Controls.Add(this.lDesc);
+            this.Controls.Add(this.lTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "P9Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "P1 Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Label lDesc;
+        private System.Windows.Forms.Button bCreate;
+    }
+}
---
+++ OODLessonProject/P9/P9Form.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace OODLessonProject.P9
+{
+    public partial class P9Form : Form
+    {
+        public P9Form()
+        {
+            InitializeComponent();
+        }
+        private void BCreate_Click(object sender, System.EventArgs ev)
+        {
+            try {
+                Program program = new Program("path");
+                MessageBox.Show("Created");
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
