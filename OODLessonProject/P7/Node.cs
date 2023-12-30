629a9742bd0cc9b8fdc3f7eb68e1dbe2643fcecc
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:07:41 +0330

    Create Node.cs

---
+++ OODLessonProject/P7/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace OODLessonProject.P7
+{
+    public class Node
+    {
+        public Student Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(Student data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
