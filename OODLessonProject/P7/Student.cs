ccf692813c7970f76c61b4dbd5f48f7cdb63bd72
Ali Tafakkori <alitafakkori82@gmail.com>
2023-12-30 11:07:39 +0330

    Create Student.cs

---
+++ OODLessonProject/P7/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace OODLessonProject.P7
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public Student(int id, string name)
+        {
+            StudentID = id;
+            Name = name;
+        }
+    }
+}
